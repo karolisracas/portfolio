df144503d98f4dbe18dd0e7361cc21e3354e5ed3
Karolis Račas <karolis.racas@gmail.com>
2020-04-16 12:06:50 +0300

    redesign hero section

--- script.js
+++ script.js
@@ -6,12 +6,15 @@ function scrollFunction() {
   let navbar = document.querySelector('.navbar');
 
   if (navbar) {
-    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
       navbar.style.height = "60px";
       navbar.style.fontSize = "1.2em";
+      navbar.style.backgroundColor = "#d7ccc8";
     } else {
+
       navbar.style.height = "100px";
       navbar.style.fontSize = "1.4em";
+      navbar.style.backgroundColor = "transparent";
     }
   } 
 }
