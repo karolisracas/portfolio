818b1bc45be8a8f0349f3b8e3296b8ad03d90260
Karolis Račas <karolis.racas@gmail.com>
2020-04-19 18:18:16 +0300

    add responsive navigation

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
 // NAVBAR SCROLL
 // When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
 window.onscroll = function() {scrollFunction()};
+hamburgerToggle();
 
 function scrollFunction() {
   let navbar = document.querySelector('.navbar');
 
   if (navbar) {
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-      navbar.style.height = "60px";
-      navbar.style.fontSize = "1.2em";
-      if (window.matchMedia("(max-width: 1024px)").matches) {
-        navbar.style.backgroundColor = "#492d15";
+    if (window.matchMedia("(min-width: 1024px)").matches) {
+      if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        navbar.style.height = "60px";
+        navbar.style.fontSize = "1.2em";
+        navbar.style.backgroundColor = "#492d15"
       } else {
-        navbar.style.backgroundColor = "#492d15";
+        navbar.style.height = "100px";
+        navbar.style.fontSize = "1.4em";
+        navbar.style.backgroundColor = "transparent";
       }
-    } else {
-      navbar.style.height = "100px";
-      navbar.style.fontSize = "1.4em";
-      navbar.style.backgroundColor = "transparent";
     }
-  } 
-}3
+  }
+}
+
+function hamburgerToggle() {
+  let hamburger = document.querySelector('.hamburger');
+  let menu = document.querySelector('.menu-container');
+
+  if (hamburger && menu) {
+    hamburger.addEventListener("click", function(){
+      menu.classList.toggle('menu-container--active');
+    });
+  }
+}
